a4417fbd66723c0737be09db596818abc6e655b4
Nam Nguyen <nam.nguyen@redspace.com>
2023-03-24 15:47:48 -0300

    🏎️

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CoolButton />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import CoolButton from "@/components/CoolButton.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    CoolButton,
   },
 };
 </script>
