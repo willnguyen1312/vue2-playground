9d8ae48480508c7a623236341ff5bfec76f578b9
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-03-29 15:28:31 -0300

    Ole

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,31 @@
 </template>
 
 <script>
-// @ is an alias to /src
+// @ts-check
+
+/**
+ * @typedef {object} Item
+ * @property {string} text
+ */
+
 import CoolButton from "@/components/CoolButton.vue";
 
 export default {
   name: "HomeView",
   components: {
     CoolButton,
+  },
+  data() {
+    return {
+      /** @type Array<Item> */
+      items: [],
+    };
+  },
+  methods: {
+    /** @param {number} input */
+    sayHi(input) {
+      this.items = [{ text: input.toString() }];
+    },
   },
 };
 </script>
